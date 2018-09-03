3da1aa74f1bc69f635cf5517dcebdcd906d401e3
cmpark0126 <cmpark@cmpark-MacBook-Air.local>
2018-09-03 10:59:19 +0900

    Update Week_02 assignment with try and catch

--- Week_02/RandomN.java
+++ Week_02/RandomN.java
@@ -1,13 +1,13 @@
 public class RandomN {
   public static void main(String[] args) {
-    if (args.length != 1)
-      System.out.println("java RandomN N");
-    else{
+    try{
       int numOfRandomNum = Integer.parseInt(args[0]);
 
       for (int cnt = 0; cnt < numOfRandomNum; cnt++) {
         System.out.println(Math.random());
       }
+    } catch (Exception e){
+      System.out.println("Usage : java RandomN N");
     }
   }
 }
